a2e01c6f95f9ab92eceb4b45b13816f7cb210e0a
Vishal Thakur <63438015+VishalThakur290@users.noreply.github.com>
2022-09-09 19:20:40 +0530

    First Build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('react'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/password-generator.github.io">
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
